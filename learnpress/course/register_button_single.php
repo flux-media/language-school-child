73dd1a08995250c59a5be4f657ccfc710cf65f91
Lee Han Kyeol <lee0hankyeol@gmail.com>
2016-04-01 17:59:39 +0900

    Added complete course support

--- learnpress/course/register_button_single.php
+++ learnpress/course/register_button_single.php
@@ -13,7 +13,12 @@ learn_press_prevent_access_directly();
 global $course;
 do_action( 'learn_press_before_course_register_button' );
 // $button_text = apply_filters( 'learn_press_take_course_button_text', esc_html__( 'Take this course', 'language-school-child' ) );
-$button_text = '무통장입금';
+
+if (get_post_meta(get_the_ID(), '_lpr_course_final', true) == 'yes') {
+	$button_text = '대기자 등록';
+} else {
+	$button_text = '무통장입금';
+}
 ?>
 <div class="cmsmasters_course_meta_item text-align-center">
 	<a id="register-course" href="<?php echo esc_url(site_url('register-for-courses?title=' . urlencode(get_the_title()) . '&price=' . 
