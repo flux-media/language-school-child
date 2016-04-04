83689316132e086a115bda224c5805de621ae9ec
Lee Han Kyeol <lee0hankyeol@gmail.com>
2016-04-04 12:21:12 +0900

    Created stand-by form page

--- learnpress/course/register_button_single.php
+++ learnpress/course/register_button_single.php
@@ -15,15 +15,17 @@ do_action( 'learn_press_before_course_register_button' );
 // $button_text = apply_filters( 'learn_press_take_course_button_text', esc_html__( 'Take this course', 'language-school-child' ) );
 
 if (get_post_meta(get_the_ID(), '_lpr_course_final', true) == 'yes') {
+	$page_slug = 'stand-by';
 	$button_text = '대기자 등록';
 } else {
+	$page_slug = 'register-for-courses';
 	$button_text = '무통장입금';
 }
 
 if (get_post_meta(get_the_ID(), '_lpr_course_condition', true) != 100) {
 ?>
 <div class="cmsmasters_course_meta_item text-align-center">
-	<a id="register-course" href="<?php echo esc_url(site_url('register-for-courses?title=' . urlencode(get_the_title()) . '&price=' . 
+	<a id="register-course" href="<?php echo esc_url(site_url($page_slug . '?title=' . urlencode(get_the_title()) . '&price=' . 
 		urlencode(get_post_meta(get_the_ID(), '_lpr_course_price', true)) . '&course_id=' . urlencode(get_the_ID()) )); ?>"
 	class="btn take-course cmsmasters_button cmsmasters_but_bg_slide_top"><?php echo $button_text;?></a>
 </div>
