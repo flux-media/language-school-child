b52cc7f850328adc2504aefcbb592899a73b86f6
Lee Han Kyeol <lee0hankyeol@gmail.com>
2016-04-28 16:06:05 +0900

    Removed Learnpress-related codes from learnpress/course/price_single.php

--- learnpress/course/price_single.php
+++ learnpress/course/price_single.php
@@ -19,31 +19,17 @@ do_action( 'learn_press_before_course_price' );
 			<?php
 			do_action( 'learn_press_begin_course_price' );
 
-			$regular_price = get_post_meta(get_the_ID(), '_lpr_course_price', true); 
+			$regular_price = get_post_meta(get_the_ID(), '_price', true);
 			$sale_price = '';
-
-			if ($regular_price == '') {
-				$regular_price = get_post_meta(get_the_ID(), '_price', true);
-				if (get_post_meta(get_the_ID(), '_sale_price', true)) {
-					$sale_price = get_post_meta(get_the_ID(), '_regular_price', true); 
-				}
-			} else {
-				$sale_price = get_post_meta(get_the_ID(), '_lpr_course_duration', true );
+			if (get_post_meta(get_the_ID(), '_sale_price', true)) {
+				$sale_price = get_post_meta(get_the_ID(), '_regular_price', true); 
 			}
 			?>
 			<?php if ($sale_price): ?>
-				<?php if ($product): ?>
-					<span class="line-through float-right">₩ <span id="before-original-price"><?php echo number_format($product->get_price_including_tax(1, $sale_price)); ?></span></span>
-				<?php else: ?>
-					<span class="line-through float-right">₩ <span id="before-original-price"><?php echo number_format($sale_price); ?></span></span>
-				<?php endif; ?>
+				<span class="line-through float-right">₩ <span id="before-original-price"><?php echo number_format($product->get_price_including_tax(1, $sale_price)); ?></span></span>
 				<br/> →
 			<?php endif; ?>
-				<?php if ($product): ?>
-					₩ <span id="original-price"><?php echo number_format($product->get_price_including_tax(1, $regular_price)); ?></span>
-				<?php else: ?>
-					₩ <span id="original-price"><?php echo number_format($regular_price); ?></span>
-				<?php endif; ?>
+			₩ <span id="original-price"><?php echo number_format($product->get_price_including_tax(1, $regular_price)); ?></span>
 			<?php do_action( 'learn_press_end_course_price' );?>
 		</span>
 	</div>
