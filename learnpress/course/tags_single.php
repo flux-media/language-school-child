b64430f5afbcba66739ef9992ec9de7cd101e2a4
Lee Han Kyeol <lee0hankyeol@gmail.com>
2016-04-28 16:17:55 +0900

    Removed Learnpress-related codes from learnpress/course/tags_single.php

--- learnpress/course/tags_single.php
+++ learnpress/course/tags_single.php
@@ -9,22 +9,14 @@
 
 learn_press_prevent_access_directly();
 do_action( 'learn_press_before_course_tags' );
-$tags = get_the_term_list( get_the_ID(), 'course_tag', '', ', ', '' );
-if ($tags == '') {
-	$tags = get_post_meta(get_the_ID(), 'as_date', true);
-}
+$tags = get_post_meta(get_the_ID(), 'as_date', true);
 ?>
 <div class="cmsmasters_course_meta_item">
 	<div class="cmsmasters_course_meta_title">
 		<span class="cmsmasters_theme_icon_lpr_duration"><?php esc_html_e('Tags', 'language-school-child'); ?></span>
 	</div>
 	<div class="cmsmasters_course_meta_info">
-<?php
-	printf(
-		'<span class="tags-links">%s</span>',	
-		$tags
-	);
-?>
+		<?php printf( '<span class="tags-links">%s</span>', $tags ); ?>
 	</div>
 </div>
 <?php
