b9f414be066006f87dda59077be568356e107bd6
Lee Han Kyeol <lee0hankyeol@gmail.com>
2016-05-19 15:29:34 +0900

    Added Facebook Pixel code

--- header.php
+++ header.php
@@ -31,6 +31,27 @@ if (is_singular() && get_option('thread_comments')) {
 
 wp_head();
 ?>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1034933649928556');
+fbq('track', 'PageView');
+<?php if (get_post_type() == 'product'): ?>
+fbq('track', 'ViewContent');
+<?php elseif (is_page('checkout')): ?>
+fbq('track', 'AddToCart');
+fbq('track', 'InitiateCheckout');
+<?php endif; ?>
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1034933649928556&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 </head>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
